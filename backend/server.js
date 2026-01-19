e38c961b86ea3c8b8dd61e2abeac841f0e44f62b
Elaine Rice <elaine.rice@gmail.com>
2026-01-19 19:30:26 +0000

    Initial project setup; initialise project, install dependencies and setup server

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Root endpoint (for testing API)
+app.get("/", (req, res) => {
+  res.send("ERR Expenses Web App API Running");
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
